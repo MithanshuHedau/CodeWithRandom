0b520c18f1ab92f6f1a91d6a51c916adb6cb69e0
Mithanshu Hedau <144697427+MithanshuHedau@users.noreply.github.com>
2025-12-03 13:41:52 +0530

    auth updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export const api = axios.create({
-  // baseURL: "https://codewithrandom.onrender.com/api",
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://codewithrandom.onrender.com/api",
+  // baseURL: "http://localhost:5000/api",
 });
 
 // Attach auth token if present in localStorage
